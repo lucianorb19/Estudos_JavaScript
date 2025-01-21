3b913c305deac36d7d637afcc29f0660f27f7551
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-01-21 12:21:52 -0300

    vetores

---
+++ aula08/vetores.js
@@ -0,0 +1,18 @@
+let letras = ['a','b','c','e','d']
+/*
+tamanho=letras.length
+let pos_letra
+for(pos_letra=0;pos_letra<=(tamanho-1);pos_letra++){
+    console.log(`Posição ${pos_letra} - valor ${letras[pos_letra]}`)
+}
+
+
+for(let pos in letras){
+    console.log(`Posição ${pos} - valor ${letras[pos]}`)
+}
+*/
+
+console.log(letras.indexOf(2))
+
+
+//F8 - Executar no console
