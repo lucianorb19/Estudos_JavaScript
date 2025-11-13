0f27b974304af636fdcff34b01ad1c45cdcc79fa
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-11-13 15:51:14 -0300

    Operações com arquivos

---
+++ Alura/Criando_primeira_biblioteca/src/index.js
@@ -0,0 +1,15 @@
+// const fs = require('fs');//USO DE FILESYSTEM
+
+// const caminhoArquivo = process.argv[2];//PEGA O QUE FOR DIGITADO NO TERMINAL APÓS 'node indes.js'
+// console.log(caminhoArquivo);
+
+// fs.readFile(caminhoArquivo, 'utf-8', (erro, texto)=>{
+//     if(erro){
+//         console.error("Erro:",erro);
+//         return;
+//     }
+//     console.log(texto);
+//     console.log(typeof texto);
+// });
+
+//flatmap ?
