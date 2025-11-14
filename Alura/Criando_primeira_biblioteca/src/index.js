d96a7c24e41b8ffcb03472eaab6c8fef3dd15458
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-11-14 10:20:18 -0300

    Operação com arquivos e tratamento de erro

--- Alura/Criando_primeira_biblioteca/src/index.js
+++ Alura/Criando_primeira_biblioteca/src/index.js
@@ -1,15 +1,45 @@
-// const fs = require('fs');//USO DE FILESYSTEM
+const fs = require('fs');
+const trataErros = require('./erros/funcoesErro');
 
-// const caminhoArquivo = process.argv[2];//PEGA O QUE FOR DIGITADO NO TERMINAL APÓS 'node indes.js'
-// console.log(caminhoArquivo);
+const caminhoArquivo = process.argv[2]; 
 
-// fs.readFile(caminhoArquivo, 'utf-8', (erro, texto)=>{
-//     if(erro){
-//         console.error("Erro:",erro);
-//         return;
-//     }
-//     console.log(texto);
-//     console.log(typeof texto);
-// });
+fs.readFile(caminhoArquivo, 'utf-8', (erro, texto) => {
+  try {
+    if (erro) throw erro
+    contaPalavras(texto);
+  } catch(erro) {
+    trataErros(erro);
+  }
+})
 
-//flatmap ?
+function contaPalavras(texto) {
+  const paragrafos = extraiParagrafos(texto)
+  //flatmap NO LUGAR DE .filter + .map
+  const contagem = paragrafos.flatMap((paragrafo) => {
+    if (!paragrafo) return [];
+    return verificaPalavrasDuplicadas(paragrafo);
+  })
+  console.log(contagem);
+}
+
+function extraiParagrafos(texto) {
+  return texto.toLowerCase().split('\n');
+}
+
+function verificaPalavrasDuplicadas(texto) {
+  const listaPalavras = texto.split(' ');
+  const resultado = {};
+  listaPalavras.forEach(palavra => {
+    if (palavra.length >= 3) {
+      const palavraLimpa = limpaPalavras(palavra);
+      resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1
+    }
+  })
+  return resultado;
+}
+
+function limpaPalavras(palavra) {
+    //EXPRESSÃO REGEX QUE RETIRA TODOS CARACTÉRES ESPECIAIS JUNTO ÀS PALAVRAS
+  return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+}
+
