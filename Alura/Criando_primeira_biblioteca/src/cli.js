8c63644b321b2c37aae07f08d7c9405ee144f547
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-11-18 16:44:22 -0300

    Biblioteca Commander

--- Alura/Criando_primeira_biblioteca/src/cli.js
+++ Alura/Criando_primeira_biblioteca/src/cli.js
@@ -1,11 +1,58 @@
 import fs from 'fs';
 //const fs = require('fs');
-
+import path from 'path';
 import trataErros from './erros/funcoesErro.js';
 //const trataErros = require('./erros/funcoesErro');
-
 import {contaPalavras} from './index.js';
 import { montaSaidaArquivo } from './helpers.js';
+//import { Command } from 'commander';
+
+
+
+
+//VERSÃO COM USO DA BIBLIOTECA COMMANDER
+/*
+function processaArquivo(texto, destino){
+  fs.readFile(texto, 'utf-8', (erro, texto) => {
+  try {
+    if (erro) throw erro
+    const resultado = contaPalavras(texto);
+    criaESalvaArquivo(resultado, destino);
+  } catch(erro) {
+    trataErros(erro);
+  }
+});
+}
+
+const program = new Command();
+program
+.version('0.0.1')
+.option('-t, --texto <string>', 'caminho do texto a ser processado')
+.option('-d  --destino <string>', 'caminho da pasta a salvar o arquivo de resultados')
+.action((options)=> {
+  const {texto, destino} = options; //DESESTRUTURAÇÃO DE ARRAY
+
+  if(!texto || !destino){
+    console.error("Erro: favor inserir caminho de origem e destino!")
+    program.help();
+    return;
+  }
+
+  //USO BIBLIOTECA PATH PARA MONTRAR CAMINHO DOS ARQUIVOS
+  caminhoTexto = path.resolve(texto);
+  caminhoDestino = path.resolve(destino);
+
+  try{
+    processaArquivo(caminhoTexto, caminhoDestino);
+    console.log("Texto processado com sucesso!");
+  }catch(erro){
+    console.log("Erro no processamento", erro)
+  }
+})
+
+program.parse();
+*/
+
 
 const caminhoArquivoLeitura = process.argv[2];
 const caminhoArquivoEscrita = process.argv[3];
